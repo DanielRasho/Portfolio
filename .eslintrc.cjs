808e70105e101db76a6467c504e98fb8b65fb238
DanielRasho <ray22933@uvg.edu.gt>
2024-05-29 16:18:29 -0600

    Feat: Add dependencias for smooth scrolling and animation.
    This are Lenis and Gsap respectively.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,8 +3,16 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  'extends': ['plugin:vue/vue3-essential', 'eslint:recommended', '@vue/eslint-config-prettier/skip-formatting', 'plugin:storybook/recommended'],
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-prettier/skip-formatting',
+    'plugin:storybook/recommended'
+  ],
   parserOptions: {
     ecmaVersion: 'latest'
+  },
+  rules: {
+    'no-unused-vars': 0
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,9 +1,34 @@
-import './assets/styles/main.css'
+import './assets/styles/03-main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
+// ANIMATION LIBRARY
+import { gsap } from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+import { Draggable } from 'gsap/Draggable'
+import { TextPlugin } from 'gsap/TextPlugin'
+// SMOOTH SCROLL
+import Lenis from 'lenis'
 
+// GSAP INITIALIZATION
+gsap.registerPlugin(ScrollTrigger, Draggable, TextPlugin)
+
+// SMOOTH SCROLL INITIALIZATION
+const lenis = new Lenis()
+//lenis.on('scroll', (e) => {
+//  console.log(e)
+//})
+
+lenis.on('scroll', ScrollTrigger.update)
+
+gsap.ticker.add((time) => {
+  lenis.raf(time * 1000)
+})
+
+gsap.ticker.lagSmoothing(0)
+
+// APP CREATION
 const app = createApp(App)
 
 app.use(createPinia())
