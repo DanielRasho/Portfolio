68b8a2ba4df94f27ce9c553af2c8df76b410ab69
DanielRasho <ray22933@uvg.edu.gt>
2024-05-29 23:55:18 -0600

    Fix: vite configuration adding base repo url

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: 'https://danielrasho.github.io/Portfolio/',
   server: {
     host: '127.0.0.1',
     port: 3000
