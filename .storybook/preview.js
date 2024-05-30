db2fe5ee4cfd87d046f3bd0fbd707ede6d6c1243
DanielRasho <ray22933@uvg.edu.gt>
2024-05-29 23:16:24 -0600

    Fix: Storybook preview to import gsap and leny on startup

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,11 @@
 /** @type { import('@storybook/vue3').Preview } */
+
+import '../src/assets/styles/03-main.css'
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+
+gsap.registerPlugin(ScrollTrigger)
+
 const preview = {
   parameters: {
     controls: {
