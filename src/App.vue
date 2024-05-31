e118da4ad7af42c035bb850922b9eb558d812fe9
DanielRasho <ray22933@uvg.edu.gt>
2024-05-30 23:50:04 -0600

    Feat: Add contacts on the footer

--- src/App.vue
+++ src/App.vue
@@ -98,12 +98,20 @@ onMounted(() => {
 
   <footer id="contacts-section">
     <div>
-      <div>PRINCIPAL</div>
+      <div><span>PRINCIPAL</span></div>
     </div>
     <div>
-      <a href="">Linkedln <i class="fa-solid fa-paperclip"></i></a>
-      <a href="">Mail <i class="fa-solid fa-paperclip"></i></a>
-      <a href="">Github <i class="fa-solid fa-paperclip"></i></a>
+      <a
+        href="https://www.linkedin.com/in/daniel-alfredo-rayo-rold%C3%A1n-38727a303/"
+        target="_blank"
+        ><span>Linkedln </span><i class="fa-solid fa-paperclip"></i
+      ></a>
+      <a href="mailto: drayoroldan@gmail.com" target="_blank"
+        ><span>Mail</span><i class="fa-solid fa-paperclip"></i
+      ></a>
+      <a href="https://github.com/DanielRasho" target="_blank"
+        ><span>Github</span> <i class="fa-solid fa-paperclip"></i
+      ></a>
     </div>
   </footer>
 </template>
@@ -112,5 +120,33 @@ onMounted(() => {
 #contacts-section {
   height: 30vh;
   background-color: var(--dark-green);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+#contacts-section > div {
+  width: 20ch;
+}
+#contacts-section a {
+  margin-top: 1rem;
+  cursor: pointer;
+}
+#contacts-section a:hover {
+  background-color: var(--white);
+}
+#contacts-section a:hover * {
+  color: var(--dark-green);
+}
+
+#contacts-section > :nth-child(2) {
+  display: flex;
+  flex-direction: column;
+}
+#contacts-section * {
+  color: var(--white);
+}
+#contacts-section span {
+  font-family: 'IBM Plex Mono';
+  font-size: 1.2rem;
 }
 </style>
