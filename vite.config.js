53485f28636f68dde545f8a25f241729146162d7
DanielRasho <ray22933@uvg.edu.gt>
2024-06-15 22:29:37 -0600

    Feat: Make vite change its 'base' property based on the running mode (production ,development)
    Due to a lack of capability to check vite builds caused by a invalid
    'base', this changes where made, so vite changes its 'base' based on the
    mode it is running with.
    
    .env.development annd .env.production contains the 'base' vale for
    running vite in 'development' and 'production' respectively.

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,26 @@
 import { fileURLToPath, URL } from 'node:url'
+import process from 'node:process'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  base: 'https://danielrasho.github.io/Portfolio/',
-  server: {
-    host: '127.0.0.1',
-    port: 3000
-  },
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ mode }) => {
+  console.log(mode)
+
+  const env = loadEnv(mode, process.cwd())
+
+  return {
+    plugins: [vue()],
+    base: env.VITE_BASE || '/',
+    server: {
+      host: '127.0.0.1',
+      port: 3000
+    },
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
     }
   }
 })
