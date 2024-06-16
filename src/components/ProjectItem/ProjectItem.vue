983a029d8b02c5303ccddc7deb2caf1590e82219
DanielRasho <ray22933@uvg.edu.gt>
2024-06-16 13:29:05 -0600

    Feat: Create ProjectItem component for displayin a project info.

---
+++ src/components/ProjectItem/ProjectItem.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="project-item">
+    <div class="project-item__title">{{ title }}</div>
+    <div class="project-item__description">
+      {{ description }}
+    </div>
+    <ImportantButton text="Link to Code" :href="link" />
+  </div>
+</template>
+
+<script setup>
+import ImportantButton from '@/components/ImportantButton/ImportantButton.vue'
+
+const props = defineProps({
+  title: { type: String },
+  description: { type: String },
+  link: { type: String }
+})
+</script>
+
+<style scoped>
+.project-item {
+  min-width: 90%;
+  margin-left: 5%;
+  margin-right: 5%;
+  display: flex;
+  flex-direction: column;
+}
+
+.project-item * {
+  color: var(--white);
+}
+
+.project-item__title {
+  font-size: 4rem;
+  font-family: 'Avigea';
+}
+
+.project-item__description {
+  font-size: 1.4rem;
+  margin-top: 2rem;
+}
+
+@media only screen and (max-width: 1040px) {
+  .project-item__title {
+    font-size: 3rem;
+  }
+  .project-item__description {
+    font-size: 1.2rem;
+    margin-top: 2rem;
+  }
+}
+</style>
