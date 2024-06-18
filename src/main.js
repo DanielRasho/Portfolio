8fcc489dba345b9c73a599f1f166bfad0ba0af80
DanielRasho <ray22933@uvg.edu.gt>
2024-06-18 12:08:13 -0600

    Fix: scroll smooth to be more stiff.

--- src/main.js
+++ src/main.js
@@ -23,7 +23,7 @@ const lenis = new Lenis()
 lenis.on('scroll', ScrollTrigger.update)
 
 gsap.ticker.add((time) => {
-  lenis.raf(time * 1000)
+  lenis.raf(time * 1500)
 })
 
 gsap.ticker.lagSmoothing(0)
